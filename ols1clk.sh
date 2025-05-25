96a152ec0ae576d8916bd4ddf6f6849c9dc751f4
Morrow Shore <97566823+MorrowShore@users.noreply.github.com>
2025-05-25 12:23:52 +0000

    attempt 3

--- ols1clk.sh
+++ ols1clk.sh
@@ -1,21 +1,4 @@
 #!/bin/bash
-##############################################################################
-#    Open LiteSpeed is an open source HTTP server.                           #
-#    Copyright (C) 2013 - 2025 LiteSpeed Technologies, Inc.                  #
-#                                                                            #
-#    This program is free software: you can redistribute it and/or modify    #
-#    it under the terms of the GNU General Public License as published by    #
-#    the Free Software Foundation, either version 3 of the License, or       #
-#    (at your option) any later version.                                     #
-#                                                                            #
-#    This program is distributed in the hope that it will be useful,         #
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of          #
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the            #
-#    GNU General Public License for more details.                            #
-#                                                                            #
-#    You should have received a copy of the GNU General Public License       #
-#    along with this program. If not, see http://www.gnu.org/licenses/.      #
-##############################################################################
 
 ###    Author: LiteSpeed
 
@@ -2517,6 +2500,12 @@ function main_init_package
     check_curl
 }
 
+
+
+if [ $# -eq 0 ]; then
+    interactive_setup
+fi
+
 function main
 {
     display_license
@@ -2542,9 +2531,6 @@ function main
     main_ols_test
 }
 
-if [ $# -eq 0 ]; then
-    interactive_setup
-fi
 while [ ! -z "${1}" ] ; do
             case "${1}" in
         --adminuser )  
